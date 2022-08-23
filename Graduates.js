f313bd34ebbab65fe16de166c14894fb8e5ef278
alfian <alfianwilfredohoris@gmail.com>
2022-08-23 16:23:43 +0700

    add a Graduates.JS

---
+++ Graduates.js
@@ -0,0 +1,10 @@
+var nama = "alfian";
+var nilai = 90;
+var absen = 5;
+
+// code here
+if (nilai > 70 && absen < 5){
+    console.log(`${nama} ` + "lulus");
+} else {
+    console.log(`${nama}` + " tidak lulus");
+}
